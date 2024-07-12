ea934992dbd0f704c0f873e0f2d4785977171bb6
Benv-B <2446823119@qq.com>
2024-07-12 21:35:35 +0800

    my_foc with some roughly c++ class setup and a rough clion project

---
+++ my_foc/cmake-build-debug/CMakeFiles/my_foc.elf.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Startup/startup_stm32f103cbtx.s" "/home/benv/robot_ben/foc/my_foc/my_foc/cmake-build-debug/CMakeFiles/my_foc.elf.dir/Startup/startup_stm32f103cbtx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/benv/robot_ben/foc/my_foc/my_foc/core/Inc"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Inc"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/core/Src/gpio.c" "CMakeFiles/my_foc.elf.dir/core/Src/gpio.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/core/Src/gpio.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/core/Src/main.c" "CMakeFiles/my_foc.elf.dir/core/Src/main.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/core/Src/main.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/core/Src/stm32f1xx_hal_msp.c" "CMakeFiles/my_foc.elf.dir/core/Src/stm32f1xx_hal_msp.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/core/Src/stm32f1xx_hal_msp.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/core/Src/stm32f1xx_it.c" "CMakeFiles/my_foc.elf.dir/core/Src/stm32f1xx_it.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/core/Src/stm32f1xx_it.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/core/Src/syscalls.c" "CMakeFiles/my_foc.elf.dir/core/Src/syscalls.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/core/Src/syscalls.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/core/Src/sysmem.c" "CMakeFiles/my_foc.elf.dir/core/Src/sysmem.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/core/Src/sysmem.c.obj.d"
+  "/home/benv/robot_ben/foc/my_foc/my_foc/core/Src/system_stm32f1xx.c" "CMakeFiles/my_foc.elf.dir/core/Src/system_stm32f1xx.c.obj" "gcc" "CMakeFiles/my_foc.elf.dir/core/Src/system_stm32f1xx.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
